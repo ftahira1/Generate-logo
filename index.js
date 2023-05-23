c815f08b0e178fbdb33498bf41c0894fc45ce864
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-05-23 10:01:50 -0400

    Installed jest and inquirer

---
+++ index.js
@@ -0,0 +1,43 @@
+//Module Dependencies 
+const fs = require("fs");
+const inquirer = require("inquirer");
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+
+
+//Initial function.
+function init () {
+    console.log(`\nHi! This is the shape and text generator!
+    \nFollow the instructions to generate a new logo.\n`);
+
+    inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+
+    inquirer.prompt([
+        {
+            type: 'maxlength-input',
+            name: 'logoText',
+            message: `\n 1) Write your logo characters. (Up to 3):`,
+            maxLength: 3
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: `2) Pick a color for your charachters. (ex: type in "blue" or "0000FF"):`,
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: `3) Choose a shape from the list below:`,
+            choices: ["square", "circle", "triangle"],
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: `4) Pick a shape color. (type in 'red' or '#FF0000'):`
+        },
+        
+    ]).then(answers => {
+        console.log(answers);
+      })
+}
+
+init();
